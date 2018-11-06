3569784f28ac31e570f0f54673010870329ff7b7
Eli Kosminsky <ekosminsky@concord.org>
2018-11-06 09:31:41 -0800

    Adds an authoring page which sets encoded JSON URL params
    URL parameters are then decoded and read on load

---
+++ src/components/authoring.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { BaseComponent, IBaseProps } from "./base";
+
+import "./app.sass";
+import Form, { ISubmitEvent } from "react-jsonschema-form";
+import { AppMode } from "../models/stores";
+import { QueryParams } from "../utilities/url-params";
+
+interface IProps extends IBaseProps {}
+interface IState {}
+
+export class AuthoringComponent extends BaseComponent<IProps, IState> {
+  public render() {
+    const onSubmit = (e: ISubmitEvent<QueryParams>) => {
+      const encodedParams = encodeURIComponent(JSON.stringify(e.formData));
+      window.open(`${location.origin}${location.pathname}?${encodedParams}`, "connected-bio-spaces");
+    };
+    return (
+      <div className="authoring">
+        <Form
+          schema={{
+            title: "Connected Bio Parameters",
+            type: "object",
+            properties: {
+              appMode: {type: "string", title: "App Mode", default: "dev", enum: [
+                "dev",
+                "authed"
+              ]}
+            }
+          }}
+          onSubmit={onSubmit} />
+      </div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,21 @@ import { AppComponent } from "./components/app";
 import { createStores } from "./models/stores";
 
 import "./index.sass";
+import { urlParams } from "./utilities/url-params";
+import { AuthoringComponent } from "./components/authoring";
 
-const stores = createStores({ });
+const stores = createStores({ appMode: urlParams.appMode });
 
-ReactDOM.render(
-  <Provider stores={stores}>
-    <AppComponent />
-  </Provider>,
-  document.getElementById("app")
-);
+if (urlParams.authoring) {
+  ReactDOM.render((
+    <AuthoringComponent />
+    ), document.getElementById("app")
+  );
+} else {
+  ReactDOM.render(
+    <Provider stores={stores}>
+      <AppComponent />
+    </Provider>,
+    document.getElementById("app")
+  );
+}
