99028761772df24208ef5c6223140ffb9bf36782
mklewandowski <matt.kain.lewandowski@gmail.com>
2018-11-08 00:08:13 -0800

    Add tests
    Add tests for changes to the UI model
    [#161766223]

--- src/models/ui.test.ts
+++ src/models/ui.test.ts
@@ -10,15 +10,18 @@ describe("ui model", () => {
   it("has default values", () => {
     expect(ui.showInvestigationModalSelect).toBe(false);
     expect(ui.showInvestigationPanel).toBe(false);
+    expect(ui.showPopulationGraph).toBe(false);
   });
 
   it("uses override values", () => {
     ui = UIModel.create({
       showInvestigationModalSelect: true,
-      showInvestigationPanel: true
+      showInvestigationPanel: true,
+      showPopulationGraph: true
     });
     expect(ui.showInvestigationModalSelect).toBe(true);
     expect(ui.showInvestigationPanel).toBe(true);
+    expect(ui.showPopulationGraph).toBe(true);
   });
 
   it("sets new values", () => {
@@ -26,6 +29,8 @@ describe("ui model", () => {
     expect(ui.showInvestigationModalSelect).toBe(true);
     ui.setShowInvestigationPanel(true);
     expect(ui.showInvestigationPanel).toBe(true);
+    ui.setShowPopulationGraph(true);
+    expect(ui.showPopulationGraph).toBe(true);
   });
 
 });
