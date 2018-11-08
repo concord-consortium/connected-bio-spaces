6201860a44af96f9fa8a7cb7c60a89fa7bd168f4
Eli Kosminsky <ekosminsky@concord.org>
2018-11-07 22:54:39 -0800

    Adds Cypress tests for authoring page and top bar URL param

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -5,8 +5,8 @@ context("Test the overall app", () => {
 
   describe("Desktop functionalities", () => {
     it("renders with text", () => {
-      cy.get('.app-container').should('exist');
-      cy.get('.nav-and-content-container').should('exist');
+      cy.get(".app-container").should("exist");
+      cy.get(".nav-and-content-container").should("exist");
     });
   });
 });
