d9339a01a30032cfab91f0302bbbe581e8d9aac4
Kevin Santos <ksantos@CC-KSANTOS.local>
2018-11-13 09:49:02 -0500

    remove additional cypress tests

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -1,30 +1,13 @@
-context("Test Default Conditions", () => {
+context("Test the overall app", () => {
 
   before(() => {
     cy.visit("");
   });
 
   describe("Desktop functionalities", () => {
-
     it("renders with text", () => {
       cy.get('.app-container').should('exist');
       cy.get('.nav-and-content-container').should('exist');
-    });
-  });
-
-  describe("Left Nav content and functionality", () => {
-
-    it("verifies connected-bio logo is present", () => {
-      cy.get('[data-test=top-bar-img]').should('be.visible')
-    });
-
-    it('verifies title is present', () => {
-      cy.get('[data-test=top-bar-title]').should('be.visible')
-    })
-
-    it("verifies investigate navigation functionality", () => {
-      cy.get('[data-test=investigate-select-button]').click()
-      cy.get('[data-test=investigate-dialog-message]').should('be.visible');
     });
   });
 });
