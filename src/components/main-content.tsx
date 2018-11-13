a0ff3e0a4753721920b48bd419f011c15ba0e66b
Sam Fentress <sfentress@concord.org>
2018-11-13 14:24:56 -0500

    Make two-up-display component generic
    This reworks the 2-up component to take ReactNodes as properties, and
    display them in the two panels.

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -4,7 +4,7 @@ import { BaseComponent, IBaseProps } from "./base";
 
 import "./main-content.sass";
 import { InvestigateDialogComponent } from "./investigate-dialog";
-import { TwoUpDisplayComponent } from "./two-up-display";
+import { PopulationsSpaceComponent } from "./spaces/populations-space";
 
 interface IProps extends IBaseProps {}
 interface IState {}
@@ -28,7 +28,7 @@ export class MainContentComponent extends BaseComponent<IProps, IState> {
     if (showInvestigationModalSelect) {
       return <InvestigateDialogComponent/>;
     } else if (showInvestigationPanel) {
-      return <TwoUpDisplayComponent/>;
+      return <PopulationsSpaceComponent/>;
     }
   }
 }
--- src/components/two-up-display.tsx
+++ src/components/two-up-display.tsx
@@ -4,7 +4,14 @@ import { BaseComponent, IBaseProps } from "./base";
 
 import "./two-up-display.sass";
 
-interface IProps extends IBaseProps {}
+interface IProps extends IBaseProps {
+  leftTitle: string;
+  leftPanel: React.ReactNode;
+  rightTitle: string;
+  rightIcon?: string;
+  onClickRightIcon?: () => void;
+  rightPanel: React.ReactNode;
+}
 interface IState {}
 
 @inject("stores")
@@ -21,40 +28,42 @@ export class TwoUpDisplayComponent extends BaseComponent<IProps, IState> {
   }
 
   private renderLeftPanel() {
+    console.log("rendering left, ", this.props.leftTitle);
     return (
       <div className="two-up-panel left-abutment" data-test="two-up-left-panel">
         <div className="header" data-test="two-up-header">
-          <div className="title" data-test="two-up-title">Investigate: Population</div>
+          <div className="title" data-test="two-up-title">{this.props.leftTitle}</div>
         </div>
-        <div className="content" data-test="two-up-content">content here</div>
+        <div className="content" data-test="two-up-content">
+          {this.props.leftPanel}
+        </div>
       </div>
     );
   }
 
   private renderRightPanel() {
-    const {ui} = this.stores;
-    const showPopulationGraph: boolean = ui.showPopulationGraph;
-    const iconId: string = showPopulationGraph ? "#icon-show-data" : "#icon-show-graph";
-    const titleText: string = showPopulationGraph ? "Graph" : "Data";
-    const contentText: string = showPopulationGraph ? "Graph goes here" :
-                                                     "Data goes here";
     return (
       <div className="two-up-panel right-abutment">
         <div className="header">
-          <div className="button-holder" onClick={this.handleClickMode}>
-            <svg className="button">
-              <use xlinkHref={iconId} />
-            </svg>
-          </div>
-          <div className="title">{titleText}</div>
+          {
+            this.props.rightIcon ?
+              <div className="button-holder" onClick={this.handleClickRightIcon}>
+                <svg className="button">
+                  <use xlinkHref={this.props.rightIcon} />
+                </svg>
+              </div>
+              : null
+            }
+          <div className="title">{this.props.rightTitle}</div>
         </div>
-        <div className="content">{contentText}</div>
+        <div className="content">
+          {this.props.rightPanel}
+        </div>
       </div>
     );
   }
 
-  private handleClickMode = () => {
-    const {ui} = this.stores;
-    ui.setShowPopulationGraph(!ui.showPopulationGraph);
+  private handleClickRightIcon = () => {
+    if (this.props.onClickRightIcon) this.props.onClickRightIcon();
   }
 }
