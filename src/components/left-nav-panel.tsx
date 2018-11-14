0df72e118bddc424fe69b88031cad08f71573e3d
mklewandowski <matt.kain.lewandowski@gmail.com>
2018-11-14 15:50:01 -0800

    Add stub collection buttons
    Add 6 stub collection buttons used to show the different collection button stylings.  Will eventually want to migrate this into a dynamic state.
    [#161596944]

--- src/components/left-nav-panel.tsx
+++ src/components/left-nav-panel.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { BaseComponent, IBaseProps } from "./base";
 
 import "./left-nav-panel.sass";
+import { CollectButtonComponent } from "./collect-button";
 import { LegendComponent } from "./legend";
 
 interface IProps extends IBaseProps {}
@@ -13,35 +14,55 @@ interface IState {}
 export class LeftNavPanelComponent extends BaseComponent<IProps, IState> {
 
   public render() {
-
     return (
       <div className="left-nav-panel">
-
         <div className="header investigate" >
         Investigate
         </div>
-
         <div className="button-holder investigate">
           <div className="button square" data-test="investigate-select-button"
             onClick={this.handleClickInvestigate}>
             select
           </div>
         </div>
-
         <div className="header collect" data-test="backpack">
         Collect
         </div>
-        <div className="button-holder collect" data-test="backpack-items">
-          <div className="button circle">+</div>
-          <div className="button circle">+</div>
-          <div className="button circle">+</div>
-          <div className="button circle">+</div>
-          <div className="button circle">+</div>
-          <div className="button circle">+</div>
-        </div>
+        {this.renderCollectButtons()}
         <div className="footer">
           <LegendComponent/>
         </div>
+      </div>
+    );
+  }
+
+  private renderCollectButtons = () => {
+    const slots = [{isCollected: true, isBrown: true, isMale: true,
+                    isHeterozygote: false, subTitle: "m"},
+                   {isCollected: true, isBrown: false, isMale: true,
+                    isHeterozygote: false, subTitle: "m2"},
+                   {isCollected: true, isBrown: true, isMale: false,
+                    isHeterozygote: false, subTitle: "f"},
+                   {isCollected: true, isBrown: false, isMale: false,
+                    isHeterozygote: false, subTitle: "f2"},
+                   {isCollected: true, isBrown: true, isMale: true,
+                    isHeterozygote: true, subTitle: "m"},
+                   {isCollected: false, isBrown: false, isMale: false,
+                    isHeterozygote: false, subTitle: ""}];
+    const buttons = slots.map((slot, index) => {
+                  return <CollectButtonComponent
+                           isCollected={slot.isCollected}
+                           isBrown={slot.isBrown}
+                           isMale={slot.isMale}
+                           isHeterozygote={slot.isHeterozygote}
+                           subTitle={slot.subTitle}
+                           index={index}
+                           key={index}
+                         />;
+                  });
+    return (
+      <div className="button-holder collect" data-test="backpack-items">
+        {buttons}
       </div>
     );
   }
@@ -50,4 +71,5 @@ export class LeftNavPanelComponent extends BaseComponent<IProps, IState> {
     const {ui} = this.stores;
     { ui.setShowInvestigationModalSelect(!ui.showInvestigationModalSelect); }
   }
+
 }
