79631effadcfd8a836b129eaaa4937bf85b93b62
Eli Kosminsky <ekosminsky@concord.org>
2018-11-15 01:50:26 -0800

    Initial integration with cell zoom model

--- src/components/spaces/populations/populations.tsx
+++ src/components/spaces/populations/populations.tsx
@@ -2,6 +2,7 @@ import { inject, observer } from "mobx-react";
 import * as React from "react";
 import { PopulationsView } from "populations-react";
 import { BaseComponent, IBaseProps } from "../../base";
+import ConnectedBio from "connected-bio";
 
 import "./populations.sass";
 
@@ -22,12 +23,13 @@ export class PopulationsComponent extends BaseComponent<IProps, IState> {
       );
       return (
         <div>
-          <PopulationsView interactive={populations.interactive} />
+          {/* <PopulationsView interactive={populations.interactive} />
           <div className="populations-toolbar">
             <button onClick={this.handleClickRunButton}>{populations.isPlaying ? "Pause" : "Run"}</button>
             <button onClick={this.handleClickResetButton}>Reset</button>
             { buttons }
-          </div>
+          </div> */}
+          <ConnectedBio />
         </div>
       );
     }
