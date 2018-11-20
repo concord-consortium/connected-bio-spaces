de78fce5323021f32994a8e2dc865fa6dc614a68
mklewandowski <matt.kain.lewandowski@gmail.com>
2018-11-20 13:19:26 -0800

    Update UI tests
    [#161784877]

--- src/models/ui.test.ts
+++ src/models/ui.test.ts
@@ -8,29 +8,24 @@ describe("ui model", () => {
   });
 
   it("has default values", () => {
-    expect(ui.showInvestigationModalSelect).toBe(false);
     expect(ui.showInvestigationPanel).toBe(false);
     expect(ui.showPopulationGraph).toBe(false);
-    expect(ui.investigationPanelSpace).toBe("populations");
+    expect(ui.investigationPanelSpace).toBe("none");
     expect(ui.availableBackpackSlots).toBe(6);
   });
 
   it("uses override values", () => {
     ui = UIModel.create({
-      showInvestigationModalSelect: true,
       showInvestigationPanel: true,
       showPopulationGraph: true,
       availableBackpackSlots: 5
     });
-    expect(ui.showInvestigationModalSelect).toBe(true);
     expect(ui.showInvestigationPanel).toBe(true);
     expect(ui.showPopulationGraph).toBe(true);
     expect(ui.availableBackpackSlots).toBe(5);
   });
 
   it("sets new values", () => {
-    ui.setShowInvestigationModalSelect(true);
-    expect(ui.showInvestigationModalSelect).toBe(true);
     ui.setShowInvestigationPanel(true);
     expect(ui.showInvestigationPanel).toBe(true);
     ui.setShowPopulationGraph(true);
