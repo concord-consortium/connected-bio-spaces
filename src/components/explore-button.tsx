0e7118107f33834023dbe589aee622700d1947c7
mklewandowski <matt.kain.lewandowski@gmail.com>
2018-11-20 13:15:15 -0800

    Add explore button component, remove obsolete investiage dialog
    Add a new explore button React component which is used to display the nav buttons in the upper left that allows users to select the current space.  Remove obsolete investiage dialog which was previously used to perform this task.
    [#161784877]

---
+++ src/components/explore-button.tsx
@@ -0,0 +1,48 @@
+import { inject, observer } from "mobx-react";
+import * as React from "react";
+import { BaseComponent, IBaseProps } from "./base";
+
+import "./explore-button.sass";
+import { Spaces } from "../models/ui";
+
+interface IProps extends IBaseProps {
+  space: Spaces;
+  title: string;
+}
+interface IState {}
+
+@inject("stores")
+@observer
+export class ExploreButtonComponent extends BaseComponent<IProps, IState> {
+
+  public render() {
+    const {ui} = this.stores;
+    let classSuffix: string = ((ui.investigationPanelSpace === this.props.space) ? "active" : "clickable");
+    switch (this.props.space) {
+      case "populations":
+        classSuffix = "population " + classSuffix;
+        break;
+      case "organism":
+        classSuffix = "organism " + classSuffix;
+        break;
+      case "breeding":
+        classSuffix = "breeding " + classSuffix;
+        break;
+    }
+    const buttonClass = "explore-button " + classSuffix;
+    const iconClass = "icon " + classSuffix;
+    const titleClass = "title " + classSuffix;
+    return (
+      <div className={buttonClass} onClick={this.handleClickButton}>
+        <div className={iconClass} />
+        <div className={titleClass}>{this.props.title}</div>
+      </div>
+    );
+  }
+
+  private handleClickButton = () => {
+    const {ui} = this.stores;
+    ui.setInvestigationPanelSpace(this.props.space);
+    ui.setShowInvestigationPanel(true);
+  }
+}
