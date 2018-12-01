a2d314666e176b92e14d2753f51cdd051a8dc2fe
Chris Hart <chart@concord.org>
2018-12-01 20:12:35 +0000

    Move CollectButton up to organism-container - this leaves cell-zoom a little empty but we haven’t yet added in any animations so I think we can leave it be for now

--- src/components/spaces/cell-zoom/organism-container.tsx
+++ src/components/spaces/cell-zoom/organism-container.tsx
@@ -1,13 +1,12 @@
 import { inject, observer } from "mobx-react";
 import * as React from "react";
 import { BaseComponent, IBaseProps } from "../../base";
-
-import "./organism-view.sass";
 import { ZoomControl } from "../../zoom-control";
 import { OrganismView } from "./organism-view";
-
+import { CollectButtonComponent } from "../../collect-button";
 import { CellZoomComponent } from "../cell-zoom/cell-zoom";
 
+import "./organism-container.sass";
 interface IProps extends IBaseProps {
   rowIndex: number;
 }
@@ -24,10 +23,19 @@ export class OrganismContainer extends BaseComponent<IProps, IState> {
   }
 
   public render() {
+    const { rowIndex } = this.props;
+    const { cellZoom } = this.stores;
+    const { cellMouse } = cellZoom.rows[rowIndex];
     const organismView = this.organismZoomedView();
 
     return (
       <div className="organism-view-container" data-test="organism-view-container">
+        <CollectButtonComponent
+          backpackMouse={cellMouse && cellMouse.backpackMouse}
+          clickMouse={this.clickMouse}
+          clickEmpty={this.clickEmpty}
+          clickClose={this.clickClose}
+        />
         {organismView}
         <ZoomControl handleZoom={this.zoomChange} />
       </div>
@@ -49,10 +57,29 @@ export class OrganismContainer extends BaseComponent<IProps, IState> {
       case 0:
         return <OrganismView rowIndex={rowIndex} />;
       case 1:
-        const organelleViewName = `organelle-wrapper${rowIndex}`;
         return <CellZoomComponent rowIndex={rowIndex} />;
       default:
         break;
     }
+  }
+
+  private clickMouse = () => {
+    // Clicks only work on empty slots
+  }
+
+  private clickEmpty = () => {
+    const { backpack, cellZoom } = this.stores;
+    const { rowIndex } = this.props;
+    const { activeMouse } = backpack;
+    if (activeMouse != null) {
+      cellZoom.setRowBackpackMouse(rowIndex, activeMouse);
+      backpack.deselectMouse();
+    }
+  }
+
+  private clickClose = () => {
+    const { cellZoom } = this.stores;
+    const { rowIndex } = this.props;
+    cellZoom.clearRowBackpackMouse(rowIndex);
   }
 }
