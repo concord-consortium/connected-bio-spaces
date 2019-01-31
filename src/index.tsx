c02f1dae6e0fa4749fec63c82eba2734ca25fd96
Sam Fentress <sfentress@concord.org>
2019-01-31 12:02:32 -0500

    Initialize student data with iframe-phone
    If we are in LARA, save data on change, and receive initial student data
    before initializing application.
    
    [#163457123]

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "mobx-react";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as iframePhone from "iframe-phone";
 import { merge } from "lodash";
 
 import { AppComponent } from "./components/app";
@@ -11,24 +12,73 @@ import { urlParams } from "./utilities/url-params";
 import { AuthoringComponent } from "./components/authoring";
 
 import "./index.sass";
+import { onSnapshot } from "mobx-state-tree";
 
-const initialStore = merge({}, defaultAuthoring, urlParams);
+// allow for migration when needed
+const STUDENT_DATA_VERSION = 1;
 
-const stores = createStores( initialStore );
+let modelInitialized = false;
 
-const appRoot = document.getElementById("app");
+function initializeModel(studentData: any) {
+  if (modelInitialized) return;
+  modelInitialized = true;
 
-if (urlParams.authoring) {
-  appRoot!.classList.add("authoring");
-  ReactDOM.render((
-    <AuthoringComponent />
-    ), appRoot
-  );
-} else {
-  ReactDOM.render(
-    <Provider stores={stores}>
-      <AppComponent showTopBar={initialStore.topBar} />
-    </Provider>,
-    appRoot
-  );
+  const initialStore = merge({}, defaultAuthoring, urlParams, studentData);
+  const stores = createStores( initialStore );
+
+  // Save data everytime backpack changes
+  onSnapshot(stores.backpack, backpackSnapshot => {
+    const saveData = {
+      version: STUDENT_DATA_VERSION,
+      backpack: {
+        collectedMice: backpackSnapshot.collectedMice
+      }
+    };
+    phone.post("interactiveState", saveData);
+  });
+
+  const appRoot = document.getElementById("app");
+
+  if (urlParams.authoring) {
+    appRoot!.classList.add("authoring");
+    ReactDOM.render((
+      <AuthoringComponent />
+      ), appRoot
+    );
+  } else {
+    ReactDOM.render(
+      <Provider stores={stores}>
+        <AppComponent showTopBar={initialStore.topBar} />
+      </Provider>,
+      appRoot
+    );
+  }
 }
+
+// If we are embedded in LARA, wait for `initInteractive` and initialize model with any student data
+const phone = iframePhone.getIFrameEndpoint();
+phone.addListener("initInteractive", (data: {
+    mode: any,
+    authoredState: any,
+    interactiveState: any,
+    linkedState: any}) => {
+  const studentData = data && (data.interactiveState || data.linkedState) || {};
+  initializeModel(studentData);
+});
+
+// When we exit page and LARA asks for student data, tell it it's already up-to-date
+phone.addListener("getInteractiveState", () => {
+  phone.post("interactiveState", "nochange");
+});
+
+phone.initialize();
+
+phone.post("supportedFeatures", {
+  apiVersion: 1,
+  features: {
+    interactiveState: true
+  }
+});
+
+// If we do not receive `initInteractive`, we are not embedded in LARA
+setTimeout( (() => initializeModel({})), 500);
