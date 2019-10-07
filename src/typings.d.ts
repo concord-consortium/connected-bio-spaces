ec4f7d39a89a201ad95d5e56297b3a65604cc9cc
Sam Fentress <sfentress@concord.org>
2019-10-07 11:07:16 -0400

    Add gifler for controlling gifs

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,6 +2,7 @@ declare module "react-dimensions";
 declare module "markdown-to-jsx";
 declare module "svg-path-properties";
 declare module "iframe-phone";
+declare module "gifler";
 
 declare interface Window {
   ActiveXObject?: any;
