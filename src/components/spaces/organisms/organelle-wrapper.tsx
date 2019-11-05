33aa1c327f2ef78003b2c55c484460156e35ef29
Sam Fentress <sfentress@concord.org>
2019-11-05 14:25:47 -0500

    "Zoom to Cell Membrane"

--- src/components/spaces/organisms/organelle-wrapper.tsx
+++ src/components/spaces/organisms/organelle-wrapper.tsx
@@ -556,7 +556,11 @@ export class OrganelleWrapper extends BaseComponent<OrganelleWrapperProps, Organ
   }
 
   private getZoomTargetLabel(target: ZoomTargetType) {
-    return "Zoom to " + target.charAt(0).toUpperCase() + target.slice(1);
+    const targetString = {
+      nucleus: "Nucleus",
+      receptor: "Cell Membrane"
+    };
+    return "Zoom to " + targetString[target];
   }
 
   private getZoomTargetFromMouseEvent(evt: any): ZoomTargetType | undefined {
