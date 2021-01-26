21b4a70b2db6d14df073a061f1e938a6651c7c16
Sam Fentress <sfentress@concord.org>
2021-01-26 11:58:32 -0500

    Create start of unit-specific species definition
    This defines the alleles and initial breeding pairs for the unit species.

--- src/models/units.ts
+++ src/models/units.ts
@@ -1,7 +1,16 @@
+import { types } from "mobx-state-tree";
 import { Unit } from "../authoring";
+
+export const UnitTypeEnum = types.enumeration("unit", ["mouse", "pea"]);
+
+export interface UnitSpecies {
+  alleles: string[];
+  breedingPairs: string[][];
+}
 
 interface UnitDefinition {
   title: string;
+  species: UnitSpecies;
   populations: {
     title: string;
   };
@@ -21,6 +30,17 @@ type Units = Record<Unit, UnitDefinition>;
 export const units: Units = {
   mouse: {
     title: "Deer Mice",
+    species: {
+      alleles: ["R", "C"],
+      breedingPairs: [
+        ["RC", "RR"],
+        ["CC", "RR"],
+        ["CC", "CC"],
+        ["RR", "RR"],
+        ["RC", "RC"],
+        ["CC", "RC"]
+      ],
+    },
     populations: {
       title: "Explore: Population",
     },
@@ -36,11 +56,22 @@ export const units: Units = {
   },
   pea: {
     title: "Peas",
+    species: {
+        alleles: ["R", "r"],
+        breedingPairs: [
+          ["Rr", "RR"],
+          ["rr", "RR"],
+          ["rr", "rr"],
+          ["RR", "RR"],
+          ["Rr", "Rr"],
+          ["rr", "Rr"]
+        ],
+      },
     populations: {
       title: "Explore: Population",
     },
     organism: {
-      title: "Explore: Population",
+      title: "Explore: Organism",
     },
     breeding: {
       title: "Greenhouse",
@@ -50,3 +81,5 @@ export const units: Units = {
     },
   }
 };
+
+export const speciesDef = (unit: Unit) => units[unit].species;
